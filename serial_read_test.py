b71e2bba68401e5b3e0c08ddce3bb62424d2d59b
huiminlim <e0036379@u.nus.edu>
2020-10-24 01:58:27 +0800

    Modify serial

--- serial_read_test.py
+++ serial_read_test.py
@@ -4,6 +4,7 @@ import serial
 
 ser = serial.Serial('COM8')
 ser.baudrate = 9600
+ser.timeout = 1000
 while True:
     ser_bytes = ser.readline()
     print(ser_bytes.decode("utf-8").replace("\r\n", ""))
