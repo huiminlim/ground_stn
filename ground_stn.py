9b1dc05077eb2ea7cf33c827e7037b31ccaef53a
huiminlim <e0036379@u.nus.edu>
2021-02-20 23:09:21 +0800

    Update mission mode transition

--- ground_stn.py
+++ ground_stn.py
@@ -1,16 +1,33 @@
 import sys
 import serial
+import time
 from multiprocessing import Process, Pipe
+
+# Note: Run this code only on Ubuntu WSL to allow multiprocessing
+# https://icircuit.net/accessing-com-port-from-wsl/2704
 
 
 # Function to call in process to collect beacons from TT&C
 def handle_incoming_beacons(serial_ttnc_obj, main_pipe):
-    print("here in process")
-    pass
+    while True:
+        # Check if pipes have anything
+        if main_pipe.poll(0.2) == True:
+            print("Pipes say something, leave now")
+            main_pipe.recv()
+            break
 
 
 # Main function to control all the ground station process transition in Mission Mode Diagram
 def main():
+
+    # Internal function to get help message
+    def get_help_message():
+        msg = ""
+        msg = msg + "To transition ground station into these modes, enter commands: " + "\n"
+        msg = msg + "Contact mode: [C] " + "\n"
+        msg = msg + "Downlink mode: [D] " + "\n"
+        msg = msg + "Command entered: "
+        return msg
 
     # Initialize serial ports for TT&C transceiver
     ttnc_port = input("Enter COM port for TT&C transceiver: ")
@@ -49,11 +66,16 @@ def main():
             print("Entering Autonomous Mode...")
             print()
             process_beacon_collection.start()
-            process_beacon_collection.join()
 
             # Wait for trigger to enter other modes
             while run_flag:
-                # print("---- WAITING FOR COMMANDS ----")
+                print("---- WAITING FOR COMMANDS ----")
+                cmd = input(get_help_message())
+                if cmd.lower() == 'c' or cmd.lower() == 'd':
+                    conn_main_process.send("stop")
+                    process_beacon_collection.join()
+                    run_flag = False
+
                 pass
 
             pass
@@ -65,9 +87,6 @@ def main():
 
     conn_main_process.close()
     conn_process_beacon.close()
-
-    # No need to close process -- daemon set
-    # process_beacon_collection.close()
 
     print("Terminated script")
     sys.exit()
